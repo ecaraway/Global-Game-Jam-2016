50b54fd6e025171dd4f11a9b9427caa7ccd3435a
jar8994 <jar8994@louisiana.edu>
2016-01-30 16:35:53 -0600

    bullet hell exited

--- Ripe Of Passage/Assets/Scripts/Bullet.cs
+++ Ripe Of Passage/Assets/Scripts/Bullet.cs
@@ -1,31 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
 
     public float speed = 1;
     private GameObject player;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         player = GameObject.FindGameObjectWithTag("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position += transform.forward*speed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    float lockPos = 0f;
+    //    transform.rotation = Quaternion.Euler(lockPos, lockPos, transform.rotation.eulerAngles.z);
+        transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -.5f);
+
+
+        transform.position += transform.forward * speed;
 
         //deletes if bullet is very far away(off screen)
         Vector3 diff = transform.position - player.transform.position;
-        if(diff.magnitude > 100f)
+        if (diff.magnitude > 100f)
             Destroy(gameObject);
-	}
+    }
 
-    void OnCollisionEnter(Collision col)
+    void OnCollisionEnter2D(Collision2D  col)
     {
         Debug.Log("Bullet hit something");
-        Destroy(gameObject);
-        
+        if(col.gameObject.name != "Player")
+        {
+            Destroy(gameObject);
+        }
+
+
     }
 }
--- Ripe Of Passage/Assets/Scripts/Shooting.cs
+++ Ripe Of Passage/Assets/Scripts/Shooting.cs
@@ -6,9 +6,14 @@ public class Shooting : MonoBehaviour {
     [SerializeField]
     private GameObject bulletPrefab;
 
+    [SerializeField]
+    private int bulletSpeed = 1;
+
     private Vector3 mousePosition;
     private Vector3 worldPosition;
     private float cameraDif;
+    private float cameraX;
+    private float cameraY;
 
     //public int speed;
 
@@ -16,7 +21,7 @@ public class Shooting : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        cameraDif = Camera.main.transform.position.y - transform.position.y;
+        cameraDif = Camera.main.transform.position.x - transform.position.x;
     }
 
     // Update is called once per frame
@@ -24,8 +29,11 @@ public class Shooting : MonoBehaviour {
     {
         //mouse controls
         worldPosition = Camera.main.ScreenPointToRay(Input.mousePosition).GetPoint(cameraDif);
+        cameraX = Input.mousePosition.x;
+        cameraY = Input.mousePosition.y;
+
         worldPosition.z = transform.position.z;
-        
+
 
         Quaternion temp = transform.rotation;
         
@@ -33,15 +41,30 @@ public class Shooting : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Left Mouse Clicked");
-            transform.LookAt(worldPosition);
-            Instantiate(bulletPrefab, transform.position + (transform.forward * .8f), transform.rotation);
+
+        //    transform.LookAt(worldPosition);
+        //    Vector3 direction = Camera.main.ScreenToWorldPoint(new Vector3(cameraX, cameraY, transform.position.z)) - transform.position;
+        //    GameObject bull = (GameObject)Instantiate(bulletPrefab, transform.position + (transform.forward * .8f), Quaternion.LookRotation(direction));
+        //    bull.GetComponent<Rigidbody2D>().AddForce(worldPosition * bulletSpeed);
+        //    bull.GetComponent<Rigidbody2D>().transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+             Vector3 myPos = transform.position;
+             float x = Input.mousePosition.x;
+             float y = Input.mousePosition.y; 
+             Vector3 direction = Camera.main.ScreenPointToRay(Input.mousePosition).GetPoint(cameraDif);
+
+             transform.LookAt(worldPosition);
+             GameObject bull = (GameObject)Instantiate(bulletPrefab, myPos + (transform.forward.normalized * .2f), transform.rotation); 
+             bull.GetComponent<Rigidbody2D>().AddForce(transform.forward * bulletSpeed);
+             bull.GetComponent<Rigidbody2D>().transform.rotation = Quaternion.LookRotation(Vector3.forward , transform.forward );
+                 //Quaternion.AngleAxis(Mathf.Atan2(transform.forward.y, transform.forward.x) * Mathf.Rad2Deg, Vector3.forward);
+
 
         }
         //arrow controls
         if(Input.GetKey(KeyCode.UpArrow)){
             Debug.Log("UpArrow Pressed");
             transform.LookAt(worldPosition);
-            Instantiate(bulletPrefab, transform.position + (transform.forward * .8f), transform.rotation);
+            Instantiate(bulletPrefab, transform.position + (transform.forward.normalized * .8f), transform.rotation);
         }
         
         if(Input.GetKey(KeyCode.DownArrow)){
