5581d45bd566392952c81ff464643d1f4593503f
jar8994 <jar8994@louisiana.edu>
2016-01-30 17:08:00 -0600

    enemy death a thing now
    no more shooting recoil

---
+++ Ripe Of Passage/Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDeath : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject deadEnemy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Bullet(Clone)")
+        {
+            Debug.Log("Enemy hit by bullet");
+            Destroy(gameObject);
+            Instantiate(deadEnemy, transform.position, Quaternion.identity);
+        }
+
+
+    }
+
+}
