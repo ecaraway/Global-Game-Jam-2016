2bbbc974c461ce870d64e6985eab33940450f3b0
jar8994 <jar8994@louisiana.edu>
2016-01-29 20:51:49 -0600

    added  fleeing behavior

---
+++ Ripe Of Passage/Assets/Scripts/FleeingBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FleeingBehavior : MonoBehaviour
+{
+
+    private GameObject player;
+    [SerializeField]
+    private float speed = 2f;
+    
+
+
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 flee = transform.position - player.transform.position;
+        flee.Normalize();
+        transform.Translate(flee * speed * Time.deltaTime);
+    }
+}
