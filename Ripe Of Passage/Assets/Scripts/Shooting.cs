92132894d56d96e094be10a9811bc555315d1122
lalooooooooo <lailadin96@gmail.com>
2016-01-30 13:21:16 -0600

    s

--- Ripe Of Passage/Assets/Scripts/Shooting.cs
+++ Ripe Of Passage/Assets/Scripts/Shooting.cs
@@ -39,7 +39,7 @@ public class Shooting : MonoBehaviour {
 
         }
         //arrow controls
-        /*if(Input.GetKey(KeyCode.UpArrow)){
+        if(Input.GetKey(KeyCode.UpArrow)){
             Debug.Log("UpArrow Pressed");
             worldPosition = Camera.main.ScreenPointToRay(Input.mousePosition).GetPoint(cameraDif);
             worldPosition.z = transform.position.z;
@@ -69,16 +69,9 @@ public class Shooting : MonoBehaviour {
               worldPosition.z = transform.position.z;
               transform.LookAt(worldPosition);
             Instantiate(bulletPrefab, transform.position + (transform.forward * .8f), transform.rotation);
-        }*/
-        
-        Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        Vector3 realDirection = Camera.main.transform.TransformDirection(movementDirection);
-// this line checks whether the player is making inputs.
-        if(realDirection.magnitude > 0.1f){
-            Quaternion newRotation = Quaternion.LookRotation(realDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
-            Instantiate(bulletPrefab, transform.position + (transform.forward * .8f), transform.rotation);
         }
+        
+        
         
 
         transform.rotation = temp;
