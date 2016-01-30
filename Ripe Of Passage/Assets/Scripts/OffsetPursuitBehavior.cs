1b6871d3d52ed0226b80b379bb73064e73a79ca7
jar8994 <jar8994@louisiana.edu>
2016-01-29 20:47:24 -0600

    add offset pursuit script for enemy

---
+++ Ripe Of Passage/Assets/Scripts/OffsetPursuitBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetPursuitBehavior : MonoBehaviour
+{
+
+  
+    private GameObject player;
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private float stopDistance = 3f;
+
+
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        Vector3 flee = player.transform.position - transform.position;
+        Vector3 direction = flee;
+        direction.Normalize();
+        if (flee.magnitude > stopDistance) 
+            transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
