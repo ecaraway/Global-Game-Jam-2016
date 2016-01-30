095c10a577caf6f3a55105b3daa537965a78d290
CarawaEG <ethan.caraway@pbrc.edu>
2016-01-30 01:04:51 -0600

    Enemy Hiding Behavior

---
+++ Ripe Of Passage/Assets/unity-movement-ai/Scripts/HideUnit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HideUnit : MonoBehaviour {
+    public Rigidbody2D target;
+
+    private SteeringBasics steeringBasics;
+    private Hide hide;
+    //private Spawner obstacleSpawner;
+	public List<Rigidbody2D> objs;
+
+    private WallAvoidance wallAvoid;
+
+    // Use this for initialization
+    void Start()
+    {
+        steeringBasics = GetComponent<SteeringBasics>();
+        hide = GetComponent<Hide>();
+        //obstacleSpawner = GameObject.Find("ObstacleSpawner").GetComponent<Spawner>();
+
+        wallAvoid = GetComponent<WallAvoidance>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 hidePosition;
+        Vector3 hideAccel = hide.getSteering(target, objs, out hidePosition);
+
+        Vector3 accel = wallAvoid.getSteering(hidePosition - transform.position);
+
+        if (accel.magnitude < 0.005f)
+        {
+            accel = hideAccel;
+        }
+
+        steeringBasics.steer(accel);
+        steeringBasics.lookWhereYoureGoing();
+    }
+}
---
+++ Ripe Of Passage/Assets/unity-movement-ai/Scripts/Movement/Hide.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(SteeringBasics))]
+[RequireComponent(typeof(Evade))]
+public class Hide : MonoBehaviour {
+    public float distanceFromBoundary = 0.6f;
+
+    private SteeringBasics steeringBasics;
+    private Evade evade;
+
+    // Use this for initialization
+    void Start () {
+        steeringBasics = GetComponent<SteeringBasics>();
+        evade = GetComponent<Evade>();
+	}
+
+    public Vector3 getSteering(Rigidbody2D target, ICollection<Rigidbody2D> obstacles)
+    {
+        Vector3 bestHidingSpot;
+        return getSteering(target, obstacles, out bestHidingSpot);
+    }
+
+    public Vector3 getSteering(Rigidbody2D target, ICollection<Rigidbody2D> obstacles, out Vector3 bestHidingSpot)
+    {
+        //Find the closest hiding spot
+        float distToClostest = Mathf.Infinity;
+        bestHidingSpot = Vector3.zero;
+
+        foreach(Rigidbody2D r in obstacles)
+        {
+            Vector3 hidingSpot = getHidingPosition(r, target);
+
+            float dist = Vector3.Distance(hidingSpot, transform.position);
+
+            if(dist < distToClostest)
+            {
+                distToClostest = dist;
+                bestHidingSpot = hidingSpot;
+            }
+        }
+
+        //If no hiding spot is found then just evade the enemy
+        if(distToClostest == Mathf.Infinity)
+        {
+            return evade.getSteering(target);
+        }
+
+        //Debug.DrawLine(transform.position, bestHidingSpot);
+
+        return steeringBasics.arrive(bestHidingSpot);
+    }
+
+    private Vector3 getHidingPosition(Rigidbody2D obstacle, Rigidbody2D target)
+    {
+        float distAway = SteeringBasics.getBoundingRadius(obstacle.transform) + distanceFromBoundary;
+
+        Vector3 dir = obstacle.position - target.position;
+        dir.Normalize();
+
+        return new Vector3 ( obstacle.position.x, obstacle.position.y, 0 ) + dir * distAway;
+    }
+}
