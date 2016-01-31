26559678de3f55117e057047ba063413e1e42d7f
CarawaEG <ethan.caraway@pbrc.edu>
2016-01-31 10:45:47 -0600

    Instructions timer

---
+++ Ripe Of Passage/Assets/Scripts/Instructions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Instructions : MonoBehaviour {
+
+	public float timeLeft = 3.0f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		timeLeft -= Time.deltaTime;
+		if ( timeLeft < 0 )
+		{
+			SceneManager.LoadScene ( "Level1" );
+		}
+		
+	}
+}
