4b69a29a3af64ce3db5ed8116a85403a347fc347
jar8994 <jar8994@louisiana.edu>
2016-01-30 22:00:34 -0600

    Revert "let me push pls"
    This reverts commit 0ff5ab17fae6dc9ea4b7d9244d507e524408bce1, reversing
    changes made to 7f8e6c07edc69ab30a4aa694d8bcffa66f717a93.

--- Ripe Of Passage/Assets/Scripts/EnemyDeath.cs
+++ Ripe Of Passage/Assets/Scripts/EnemyDeath.cs
@@ -5,15 +5,12 @@ public class EnemyDeath : MonoBehaviour {
 
     [SerializeField]
     private GameObject deadEnemy;
-    private GameManager gm;
-
 
 	public RoomManager room;
 
 	// Use this for initialization
 	void Start () {
-        gm.numOfEnemies++;
-
+	
 	}
 	
 	// Update is called once per frame
@@ -25,12 +22,8 @@ public class EnemyDeath : MonoBehaviour {
     {
         if (col.gameObject.name == "Bullet(Clone)")
         {
-<<<<<<< HEAD
-            Debug.Log("Enemy hit by bullet");        
-=======
             Debug.Log("Enemy hit by bullet");
 			room.OnEnemyKill ( );
->>>>>>> 7f8e6c07edc69ab30a4aa694d8bcffa66f717a93
             Destroy(gameObject);
             Instantiate(deadEnemy, transform.position, Quaternion.identity);
         }
